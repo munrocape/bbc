832a6baff41a2eca8dc19361afc74ec89b21b34e
Zach Munro-Cape <zach.munrocape@gmail.com>
2015-03-21 00:06:00 -0300

    Return the URI and not the argument

--- bbcclient/client.go
+++ bbcclient/client.go
@@ -99,9 +99,11 @@ func (c *Client) GetPretty(category string) string {
 
 func (c *Client) GetUrl(category string) string {
 	if _, ok := c.NewsCategories[category]; ok {
-		return fmt.Sprintf("http://www.bbc.com/news/%s", category)
+		uri := c.NewsCategories[category].Uri
+		return fmt.Sprintf("http://www.bbc.com/news/%s", uri)
 	} else if _, ok := c.SportsCategories[category]; ok {
-		return fmt.Sprintf("http://www.bbc.com/sport/0/%s", category)
+		uri := c.SportsCategories[category].Uri
+		return fmt.Sprintf("http://www.bbc.com/sport/0/%s", uri)
 	} else {
 		return ""
 	}
